4576c9711147e719b3f1a9d20590d02b5a6295a1
HitalloExiled <hitalloexiled@hotmail.com>
2017-11-10 14:00:30 -0200

    Install task now ignores @surface modules.

--- tasks/install.js
+++ tasks/install.js
@@ -8,18 +8,31 @@ let commands = [];
 for (let $module of modules)
 {
     let source = path.normalize(path.join(paths.modules, $module.name));
-    commands.push(common.execute(`Installing ${$module.name}`, `cd ${source} && npm install`));
+
+    let dependencies = require(path.join(source, 'package.json')).dependencies || {};
+
+    let targets = Object.keys(dependencies)
+        .filter(x => !x.startsWith('@surface/'))
+        .map(key => `${key}@${dependencies[key]}`)
+        .join(' ');
+
+    if (targets)
+        commands.push(common.execute(`${$module.name} dependencies installed.`, `cd ${source} && npm install ${targets} --save-exact`));
 }
 
-Promise.all(commands).then
-(
-    () => Promise.all
-    (
-        [
-            common.execute(`Installing Client`, `cd ${paths.client} && npm install`),
-            common.execute(`Installing Server`, `cd ${paths.server} && npm install`),
-            common.execute(`Installing Tests`,  `cd ${paths.tests} && npm install`)
-        ]
-    )
-    .then(() =>console.log('\nDone!'))
-);
+for (let targetPath of [paths.client, paths.server, paths.tests])
+{
+    let source = path.join(targetPath, '../');
+
+    let dependencies = require(path.join(source, 'package.json')).dependencies || {};
+
+    let targets = Object.keys(dependencies)
+        .filter(x => !x.startsWith('@surface/'))
+        .map(key => `${key}@${dependencies[key]}`)
+        .join(' ');
+
+    if (targets)
+        commands.push(common.execute(`${path.parse(path.resolve(targetPath, '../')).name} dependencies installed.`, `cd ${source} && npm install ${targets} --save-exact`));
+}
+
+Promise.all(commands).then(() =>console.log('\nDone!'));
