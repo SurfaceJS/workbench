27cc2d8c6f240a7ccc5bac36f1fe57f1ef742960
HitalloExiled <hitalloexiled@hotmail.com>
2017-11-13 19:46:51 -0200

    Tests migrated from 'SurfaceJS/workbench' to 'SurfaceJS/modules' repository.

--- tests/source/collection.ts
+++
@@ -1,8 +0,0 @@
-import { Dictionary } from '@surface/collection';
-
-export function execute()
-{
-    let dictionary = new Dictionary({ foo: 1, bar: 2});
-    
-    dictionary.forEach(x => console.log(x.key));
-}
