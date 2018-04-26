23fb48dd48a8a31817b66dd1a57427a3d4af00dc
HitalloExiled <hitalloexiled@hotmail.com>
2018-04-25 21:31:42 -0300

    Added common tests.

--- tasks/patterns.ts
+++ tasks/patterns.ts
@@ -3,6 +3,6 @@ export default
     clean:
     {
         include: /\.(js(\.map)?|d\.ts)$/,
-        exclude: /@?types|core|type-root|lazy-loader|node_modules|interfaces/
+        exclude: /@?types|core|fixtures|type-root|lazy-loader|node_modules|interfaces/
     }
 };
