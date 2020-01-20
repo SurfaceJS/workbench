3aa1d379426659442df89a93f999e64ee099c01d
Rafael França <hitalloexiled@hotmail.com>
2020-01-19 22:04:28 -0300

    feature: adds themes

--- client/source/app/index.ts
+++ client/source/app/index.ts
@@ -1,5 +1,7 @@
 import CustomElement from "@surface/custom-element";
 import { element }   from "@surface/custom-element/decorators";
+import ColorBucket   from "@surface/material-design/color-bucket";
+import ThemeHandler  from "@surface/material-design/theme-handler";
 import Router        from "@surface/router";
 import ViewHost      from "@surface/view-host";
 import ViewManager   from "@surface/view-manager";
@@ -7,21 +9,30 @@ import template      from "./index.html";
 import style         from "./index.scss?raw";
 import { load }      from "./module-loader";
 
+ColorBucket.use({ light: { secondary: "lime", text: "white" }, dark: { secondary: "orange", text: "gray" } });
+ColorBucket.initialize();
+
 @element("app-root", template, style)
 export class App extends CustomElement
 {
+    private readonly _themehadler = new ThemeHandler();
+
     private viewManager!: ViewManager;
 
-    private _data: string = "";
-    public get data(): string
+    private _dark: boolean = false;
+    public get dark(): boolean
     {
-        return this._data;
+        return this._dark;
     }
 
-    public set data(value: string)
+    public set dark(value: boolean)
     {
-        this._data = value;
+        this._dark = value;
+
+        this._themehadler.toogle();
     }
+
+    public data: string = "";
 
     public constructor()
     {
