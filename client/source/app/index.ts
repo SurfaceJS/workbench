fb312dc47d329aa0451953d1450a809e9e93c1da
Rafael França <hitalloexiled@hotmail.com>
2020-07-14 11:22:24 -0300

    feature: Implements new router

--- client/source/app/index.ts
+++ client/source/app/index.ts
@@ -1,8 +1,6 @@
 import CustomElement, { element } from "@surface/custom-element";
-import ViewRouter                 from "@surface/view-router";
 import template                   from "./index.html";
 import style                      from "./index.scss";
-import routes                     from "./routes";
 
 @element("app-root", template, style)
 export class App extends CustomElement
@@ -16,12 +14,5 @@ export class App extends CustomElement
     public set data(value: string)
     {
         this._data = value;
-    }
-
-    public constructor()
-    {
-        super();
-
-        ViewRouter.registerDirective(new ViewRouter(routes));
     }
 }
--- client/source/components/dummy-parent/index.ts
+++ client/source/components/dummy-parent/index.ts
@@ -1,5 +1,3 @@
-import "@surface/components/stack-panel";
-
 import CustomElement, { element } from "@surface/custom-element";
 import template                   from "./index.html";
 import style                      from "./index.scss";
