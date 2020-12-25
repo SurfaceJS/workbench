1db97dae1b7ce4acdecf3aa38686d47225ffb323
Rafael França <hitalloexiled@hotmail.com>
2020-12-25 18:16:46 -0300

    feature: Adds manifest json.

--- client/source/index.ts
+++ client/source/index.ts
@@ -2,6 +2,8 @@ import CustomElement from "@surface/custom-element";
 import WebRouter     from "@surface/web-router";
 import routes        from "./routes";
 
+window.addEventListener("load", async () => navigator.serviceWorker.register("/service-worker.js"));
+
 const router = new WebRouter("app-root", routes);
 
 CustomElement.registerDirective(WebRouter.createDirectiveRegistry(router));
